34a4f01d1035a1d5a5cc316a8f47dad01e4f0ca6
David Royer <droyer01@gmail.com>
2018-12-15 02:16:32 -0500

    update

--- config/site.js
+++ config/site.js
@@ -1,14 +1,17 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
-  analyticsID: '123',
+  lang: 'en',
+  url: 'https://nuxt-boilerplate.netlify.com',
+
   title: 'Nuxt Boilerplate - Starting Point For Websites', // Navigation and Site Title
-  shortName: 'Nuxt Boilerplate', // Alternative Site title for SEO
-  // url: 'https://www.davidroyer.me', // Domain of your site. No trailing slash!
   description:
     'The site description will go here for SEO and Social Media Sharing',
   ogImage: 'share.png',
-  lang: 'en',
-  url: 'https://nuxt-boilerplate.netlify.com',
+
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  analyticsID: '123',
+  shortName: 'Nuxt Boilerplate', // Alternative Site title for SEO
+  // url: 'https://www.davidroyer.me', // Domain of your site. No trailing slash!
+
   fontAwesomeIcons: {
     brands: ['faTwitter', 'faGithub', 'faLinkedinIn'],
     regular: ['faTimesCircle'],
